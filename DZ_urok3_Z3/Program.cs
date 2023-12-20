4b0235fc3a8e42f5c8471f664cf00b6bac103455
RomanPohNah <147521270+RomanPohNah@users.noreply.github.com>
2023-12-20 22:11:30 +0900

    Add files via upload

---
+++ DZ_urok3_Z3/Program.cs
@@ -0,0 +1,18 @@
+﻿double[] array = { 2.2, 8.6, 3.3, 4.4, 5.5};
+double maxNum = array[0];
+double minNum = array[0];
+for(int i = 0; i < array.Length; i++)
+{
+    if (array[i] > maxNum)
+    {
+      maxNum = array[i];  
+    }
+    if (array[i] > minNum)
+    {
+      minNum = array[i];  
+    }
+}
+double difference = maxNum - minNum;
+Console.WriteLine(maxNum);
+Console.WriteLine(minNum);
+Console.WriteLine(difference);
